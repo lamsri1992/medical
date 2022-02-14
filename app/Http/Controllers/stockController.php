8bd57c53ba992fbb907d24204e9c0814c61c0776
lamsri1992 <satiya3577@gmail.com>
2022-02-14 12:47:40 +0700

    Update stock status view

--- app/Http/Controllers/stockController.php
+++ app/Http/Controllers/stockController.php
@@ -12,6 +12,7 @@ class stockController extends Controller
         $result = DB::connection(session('database'))->select(DB::raw("SELECT *, SUM(medical_store.store_amount) AS amount
                     FROM medical_store
                     LEFT JOIN medical_data ON medical_data.med_code = medical_store.store_med_code
+                    WHERE medical_data.med_status = 'Y'
                     GROUP BY medical_store.store_med_code"));
         // return dd($result);
         return view('stock.index',['result'=>$result]);
